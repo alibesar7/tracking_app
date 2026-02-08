4d007f589484159e5ab52e5b6c5d8a5f8fe84f37
alibesar7 <alibesar28@gmail.com>
2026-02-08 16:06:57 +0200

    chore(API-1): make main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,29 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:tracking_app/app/config/di/di.dart';
+import 'package:tracking_app/app/core/firebase/cloud_messaging.dart';
 import 'package:tracking_app/firebase_options.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  configureDependencies();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const TrackingApp());
+  FirebaseMessaging.onBackgroundMessage(
+    CloudMessaging.firebaseMessagingBackgroundHandler,
+  );
+  await CloudMessaging.setupFlutterNotifications();
+  CloudMessaging.printDeviceToken();
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('ar')],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en'),
+      child: const TrackingApp(),
+    ),
+  );
 }
 
 class TrackingApp extends StatelessWidget {
