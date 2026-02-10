4dcbab8eee308794e79507ee40bf26de7a1bb17f
mariam <mariemmohamed2030@gmail.com>
2026-02-10 21:16:07 +0200

    feat(SCRUM-73): add presentation layer for change password using cubit

--- lib/app/core/router/app_router.dart
+++ lib/app/core/router/app_router.dart
@@ -1,3 +1,13 @@
 import 'package:go_router/go_router.dart';
+import 'package:tracking_app/app/core/router/route_names.dart';
+import 'package:tracking_app/features/auth/presentation/reset_password/pages/change_password_page.dart';
 
-final GoRouter appRouter = GoRouter(routes: []);
+final GoRouter appRouter = GoRouter(
+  initialLocation: RouteNames.changePassword,
+  routes: [
+    GoRoute(
+      path: RouteNames.changePassword,
+      builder: (context, state) => const ChangePasswordPage(),
+    ),
+  ],
+);
