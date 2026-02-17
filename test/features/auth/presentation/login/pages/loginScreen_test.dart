fa9bddfa456cb947d9c05791ca08df932f312d10
alibesar7 <alibesar28@gmail.com>
2026-02-17 15:29:57 +0200

    chore(API-1): merge profile to dev and fix tests

--- test/features/auth/presentation/login/pages/loginScreen_test.dart
+++ test/features/auth/presentation/login/pages/loginScreen_test.dart
@@ -1,12 +1,15 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mockito/annotations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tracking_app/app/config/auth_storage/auth_storage.dart';
 import 'package:tracking_app/features/auth/domain/repos/auth_repo.dart';
 import 'package:tracking_app/features/auth/domain/usecase/login_usecase.dart';
 import 'package:tracking_app/features/auth/presentation/login/manager/login_cubit.dart';
 import 'package:tracking_app/features/auth/presentation/login/pages/loginScreen.dart';
+import 'package:tracking_app/generated/locale_keys.g.dart';
 
 import 'loginScreen_test.mocks.dart';
 
@@ -17,6 +20,11 @@ void main() {
   late LoginUseCase loginUseCase;
   late LoginCubit loginCubit;
   late GetIt getIt;
+
+  setUpAll(() async {
+    SharedPreferences.setMockInitialValues({});
+    await EasyLocalization.ensureInitialized();
+  });
 
   setUp(() {
     getIt = GetIt.instance;
@@ -38,17 +46,23 @@ void main() {
   });
 
   Widget createWidgetUnderTest() {
-    return MaterialApp(home: const LoginScreen());
+    return EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('ar')],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en'),
+      child: const MaterialApp(home: LoginScreen()),
+    );
   }
 
   testWidgets('LoginScreen renders correctly', (WidgetTester tester) async {
     // Act
     await tester.pumpWidget(createWidgetUnderTest());
+    await tester.pumpAndSettle();
 
     // Assert
-    expect(find.text('email'), findsOneWidget);
-    expect(find.text('password'), findsOneWidget);
-    expect(find.text('continueTxt'), findsOneWidget);
+    expect(find.text(LocaleKeys.email), findsOneWidget);
+    expect(find.text(LocaleKeys.password), findsOneWidget);
+    expect(find.text(LocaleKeys.login), findsNWidgets(2));
   });
 
   testWidgets('Enters text into email and password fields', (
@@ -56,6 +70,8 @@ void main() {
   ) async {
     // Act
     await tester.pumpWidget(createWidgetUnderTest());
+    await tester.pumpAndSettle();
+
     await tester.enterText(find.byType(TextFormField).first, 'test@test.com');
     await tester.enterText(find.byType(TextFormField).last, 'password123');
     await tester.pump();
