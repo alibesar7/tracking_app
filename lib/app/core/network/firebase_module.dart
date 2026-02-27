92ccd6c962374b11596904910d817ada69701f22
alibesar7 <alibesar28@gmail.com>
2026-02-27 23:18:20 +0200

    chore(API-1): fix d

--- lib/app/core/network/firebase_module.dart
+++ lib/app/core/network/firebase_module.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:injectable/injectable.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:injectable/injectable.dart';
 
 @module
 abstract class FirebaseModule {
