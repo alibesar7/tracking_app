932163ed4b03d753e4141513892f2bb0fecd3fb7
mariam <mariemmohamed2030@gmail.com>
2026-03-04 21:46:41 +0200

    feat(SCRUM-98): enhance location page with dynamic destination setting and real-time route fetching

--- lib/features/driver_orders_details/presentation/pages/location_page.dart
+++ lib/features/driver_orders_details/presentation/pages/location_page.dart
@@ -18,50 +18,19 @@ class _LocationPageState extends State<LocationPage> {
   final MarkerWidgetsController _controller = MarkerWidgetsController();
 
   final LatLng myLocation = LatLng(31.2515108, 29.9842777);
-  final LatLng destination = LatLng(31.1923215, 29.9162657);
+  LatLng? destination;
+  final String myAddress = "City Centre Alexandria";
 
   Set<Polyline> polylines = {};
 
   @override
   void initState() {
     super.initState();
-    _addMarkers();
-    _getRealRoute();
+    _addMyMarker();
+    _setDestinationFromAddress();
   }
 
-  Future<void> _getRealRoute() async {
-    final url =
-        'https://router.project-osrm.org/route/v1/driving/${myLocation.longitude},${myLocation.latitude};${destination.longitude},${destination.latitude}?overview=full&geometries=polyline';
-    final response = await http.get(Uri.parse(url));
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-
-      if (data['code'] == 'Ok') {
-        String encodedPolyline = data['routes'][0]['geometry'];
-
-        List<PointLatLng> result = PolylinePoints.decodePolyline(
-          encodedPolyline,
-        );
-
-        List<LatLng> polylineCoordinates = result
-            .map((point) => LatLng(point.latitude, point.longitude))
-            .toList();
-
-        setState(() {
-          polylines = {
-            Polyline(
-              polylineId: const PolylineId("real_route"),
-              color: Colors.pink,
-              width: 5,
-              points: polylineCoordinates,
-            ),
-          };
-        });
-      }
-    }
-  }
-
-  void _addMarkers() {
+  void _addMyMarker() {
     _controller.addMarkerWidget(
       markerWidget: MarkerWidget(
         markerId: const MarkerId("driver_location"),
@@ -79,20 +48,83 @@ class _LocationPageState extends State<LocationPage> {
         position: myLocation,
       ),
     );
+  }
 
-    _controller.addMarkerWidget(
-      markerWidget: MarkerWidget(
-        markerId: const MarkerId("user_location"),
-        child: customMarker(
-          "User",
-          const Icon(Icons.home_outlined, color: AppColors.pink, size: 20),
+  Future<void> _setDestinationFromAddress() async {
+    LatLng? result = await getLatLngFromAddress(myAddress);
+    if (result != null) {
+      destination = result;
+      _controller.addMarkerWidget(
+        markerWidget: MarkerWidget(
+          markerId: const MarkerId("destination_location"),
+          child: customMarker(
+            "Destination",
+            const Icon(Icons.home_outlined, color: AppColors.pink, size: 20),
+          ),
         ),
-      ),
-      marker: Marker(
-        markerId: const MarkerId("user_location"),
-        position: destination,
-      ),
+        marker: Marker(
+          markerId: const MarkerId("destination_location"),
+          position: result,
+        ),
+      );
+      await _getRealRoute();
+    }
+  }
+
+  Future<LatLng?> getLatLngFromAddress(String address) async {
+    final url =
+        "https://nominatim.openstreetmap.org/search?q=${Uri.encodeComponent("$address, Egypt")}&format=json&limit=1&addressdetails=1";
+
+    final response = await http.get(
+      Uri.parse(url),
+      headers: {"User-Agent": "tracking_app"},
     );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      print("<<<<<<<< Geocode response: $data");
+
+      if (data.isNotEmpty) {
+        double lat = double.parse(data[0]['lat']);
+        double lon = double.parse(data[0]['lon']);
+        return LatLng(lat, lon);
+      }
+    }
+
+    return null;
+  }
+
+  Future<void> _getRealRoute() async {
+    if (destination == null) return;
+    final url =
+        'https://router.project-osrm.org/route/v1/driving/${myLocation.longitude},${myLocation.latitude};${destination!.longitude},${destination!.latitude}?overview=full&geometries=polyline';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      print('<<<<<<<<< OSRM data: $data');
+      if (data['code'] == 'Ok') {
+        String encodedPolyline = data['routes'][0]['geometry'];
+        List<PointLatLng> result = PolylinePoints.decodePolyline(
+          encodedPolyline,
+        );
+        List<LatLng> polylineCoordinates = result
+            .map((point) => LatLng(point.latitude, point.longitude))
+            .toList();
+
+        setState(() {
+          polylines = {
+            Polyline(
+              polylineId: const PolylineId("real_route"),
+              color: Colors.pink,
+              width: 5,
+              points: polylineCoordinates,
+            ),
+          };
+        });
+      } else {
+        print("OSRM Error: ${data['code']}");
+      }
+    }
   }
 
   CameraPosition routeCameraPosition = const CameraPosition(
